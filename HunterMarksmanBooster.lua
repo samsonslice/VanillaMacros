3be6c16e684d19dd5c7244ecc11aa985ded7502b
samsonslice <samsonslice@users.noreply.github.com>
2017-05-25 23:50:25 -0500

    hunter new

--- HunterMarksmanBooster.lua
+++ HunterMarksmanBooster.lua
@@ -2,7 +2,7 @@
 /script if not buffed("Aspect of the Hawk", "player") then cast("Aspect of the Hawk"); end
 /script if not buffed("Trueshot Aura", "player") then cast("Trueshot Aura"); end
 /script if UnitIsTapped("target") then if not IsAutoRepeatAction(36) then CastSpellByName("Auto Shot"); end end
-/script if UnitIsTapped("target") then cast("Arcane Shot"); end
+/script if UnitIsTapped("target") then cast("Arcane Shot") cast("Wing Clip"); end
 /script if UnitIsUnit("targettarget", "player") then CastSpellByName("Feign Death"); end
 /script if UnitIsTapped("target") then PetAttack(); end
 /follow shadeshade
